b4ee2bf5aeb5c7e0baf0166763fd37b23c4754c1
Kameron <122124504+Kameronjan@users.noreply.github.com>
2023-08-11 16:45:51 -0400

    added in a switch statement as well as tested the code to ensure that it is working properly, good to go

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -7,3 +7,21 @@ const getUserChoice = userInput => {
     console.log('Error! Please input a valid entry.')
   }
 };
+
+function getComputerChoice() {
+  number = Math.floor(Math.random() * 3);
+
+  switch (number) {
+    case 0:
+      return 'rock';
+      break;
+    case 1:
+      return 'paper';
+      break;
+    case 2:
+      return ' scissors';
+      break;
+  }
+};
+
+console.log(getComputerChoice());
