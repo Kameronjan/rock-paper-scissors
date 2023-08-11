e6112d1cacc99363402580ed2ca7176ed5102d2c
Kameron <122124504+Kameronjan@users.noreply.github.com>
2023-08-11 16:37:59 -0400

    added in an if/else statement so that if the user choses a correct option, we will return userInput but if not, we will provide them with an error message

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,2 +1,9 @@
-const getUserChoice => userInput{};
+const getUserChoice => userInput{
+  if (userInput === 'rock' || userInput === 'paper' || userInput ==='scissors') {
+    return userInput;
+  } else {
+    console.log('Error! Please input a valid entry.')
+  }
+};
+
 userInput = userInput.toLowerCase();
